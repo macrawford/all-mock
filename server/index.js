467c2dadff14f8c8509d0cdeaa0971edb4da1dc0
Matthew Crawford <matthewcrawford@Matthews-Air.home>
2021-11-18 16:55:37 -0800

    Set up React app with Node and Express

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`App running on localhost: ${PORT}`)
+})
---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>REACT WORKING</div>
+    )
+};
+
+export default App;

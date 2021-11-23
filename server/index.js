34d030941cd902025250b60c51cc7036799f52ed
Matthew Crawford <matthewcrawford@Matthews-Air.home>
2021-11-22 16:07:14 -0800

    Implemented search functionality as get request to thecocktaildb api

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const axios = require('axios')
 
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/search/:search', (req, res) => {
+    const { search } = req.params
+    console.log('search: ', search)
+    // COULDN'T FIGURE OUT WHY PARAMS WEREN'T WORKING
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
+        .then(({data}) => {
+            console.log('Should have cocktails here! ', data)
+        })
+        .catch(error => {
+            console.log('error: ', error)
+        })
+})
 
 const PORT = 3000 || process.env.PORT;
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 function App() {
+    const [resultsObject, setResultsObject] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+    const handleChange = (e) => {
+        setSearchInput(e.target.value);
+        console.log('searchInput: ', searchInput)
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.get('/search/' + searchInput)
+            .then(({data}) => {
+                console.log('data: ', data)
+                // add it to state
+            })
+            .catch((error) => {
+                console.log('error: ', error)
+            })
+    };
     return (
-        <div>REACT WORKING</div>
+        <div>
+            <h1>
+                Your Own Personal Bartender!
+            </h1>
+            <button style={{marginBottom: 15}}>
+                Show your saved cocktails
+            </button>
+            <form>
+                <input onChange={handleChange}></input>
+                <button onClick={handleSubmit}>Search Cocktails</button>
+            </form>
+        </div>
     )
 };
 
